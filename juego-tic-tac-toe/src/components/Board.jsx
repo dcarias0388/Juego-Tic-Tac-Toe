aa3313b865525c551b652a5a80af5703f55e5a4c
DailenyAP <dcarias0388@gmail.com>
2021-06-17 00:01:54 -1100

    Ejercicio terminado

--- juego-tic-tac-toe/src/components/Board.jsx
+++ juego-tic-tac-toe/src/components/Board.jsx
@@ -3,36 +3,18 @@ import "../index.css";
 import Square from "./Square";
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    alert("llego al onclick");
-    const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
-  }
-
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onclick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const status = "Next player: X";
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
